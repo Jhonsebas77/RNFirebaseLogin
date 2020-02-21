771d580ef272db5d90516a13058089a932eb4183
Sebastian Otalora MB-JSOB <jhonsebas77@outlook.com>
2020-02-20 23:38:45 -0500

    Add Firebase/Analytics in IOS

--- ios/RNFirebaseLogin/AppDelegate.m
+++ ios/RNFirebaseLogin/AppDelegate.m
@@ -10,11 +10,13 @@
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import <Firebase.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [FIRApp configure];
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"RNFirebaseLogin"
@@ -38,5 +40,4 @@
   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
 }
-
 @end
